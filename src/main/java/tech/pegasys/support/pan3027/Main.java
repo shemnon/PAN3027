8b6249864506e344af54e1bc5b37e4bc5e1a557d
shemnon <danno.ferrin@gmail.com>
2019-08-16 17:41:29 -0600

    Better reproduction.  Dont' call SS2 directly, do it through SS1

--- src/main/java/tech/pegasys/support/pan3027/Main.java
+++ src/main/java/tech/pegasys/support/pan3027/Main.java
@@ -3,9 +3,11 @@ package tech.pegasys.support.pan3027;
 import tech.pegasys.pantheon.Pantheon;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.Arrays;
 
 import com.google.common.io.Files;
+import okhttp3.OkHttpClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.web3j.crypto.Credentials;
@@ -13,7 +15,6 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.simplestorage.SimpleStorage;
-import org.web3j.simplestorage2.SimpleStorage2;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Numeric;
@@ -24,17 +25,28 @@ public class Main {
   private static String PRIVATE_KEY_1 =
       "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63";
 
-  public static void main(final String[] args) throws Exception {
+  public static void main(final String[] args) {
     final File dataPath = Files.createTempDir();
 
     final int jsonRpcPort = 18545;
+
+    // for debugging
+    System.setProperty("vertx.options.blockedThreadCheckInterval", "200000000");
+    final HttpService httpService =
+        new HttpService(
+            "http://127.0.0.1:" + jsonRpcPort,
+            new OkHttpClient.Builder()
+                .callTimeout(Duration.ofHours(1))
+                .readTimeout(Duration.ofHours(1))
+                .writeTimeout(Duration.ofHours(1))
+                .build());
 
     startPantheon(dataPath, jsonRpcPort);
 
     try {
       Thread.sleep(5000); // startup
 
-      final Web3j web3j = Web3j.build(new HttpService("http://127.0.0.1:" + jsonRpcPort));
+      final Web3j web3j = Web3j.build(httpService);
       LOG.info(
           "Connected to Ethereum client version: "
               + web3j.web3ClientVersion().send().getWeb3ClientVersion());
@@ -54,21 +66,22 @@ public class Main {
       final String address = simpleStorage.getAddress().send();
       LOG.info("Created contract address {}", address);
 
-      final SimpleStorage2 ss2 =
-          SimpleStorage2.load(address, web3j, credentials, contractGasProvider);
-
       final byte[] first = new byte[32];
       final byte[] second = new byte[32];
       Arrays.fill(first, (byte) 1);
       Arrays.fill(second, (byte) 2);
-      final TransactionReceipt setTx = ss2.set(first, second).send();
-      LOG.info("SimpleStorage2.set result {}", setTx.getStatus());
 
-      final var result = ss2.get().send();
+      final TransactionReceipt ss1SetVales = simpleStorage.set(first, second).send();
+      LOG.info("SimpleStorage.setValue result {}", ss1SetVales.getStatus());
+
+      final var ss1GetValues = simpleStorage.getInstance().send();
       LOG.info(
-          "SimpleStorage2.get result {}, {}",
-          Numeric.toHexStringNoPrefix(result.getValue1()),
-          Numeric.toHexStringNoPrefix(result.getValue2()));
+          "SimpleStorage.getInstance result address={} value1={}  value2={}",
+          ss1GetValues.getValue1(),
+          Numeric.toHexStringNoPrefix(ss1GetValues.getValue2()),
+          Numeric.toHexStringNoPrefix(ss1GetValues.getValue3()));
+    } catch (final Exception e) {
+      e.printStackTrace(System.out);
     } finally {
       System.exit(0);
     }
